34372dd3cd660c81ff5af752f646066c9e9b9631
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-07-12 21:43:57 -0700

    final version of script; added txt file output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,6 +10,7 @@ with open(csvpath, encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     csv_header = next(reader)
 
+    # create the dictionary and set the count variables to 0
     vote_count = {}
     current_vote = 0
     tally = 0
@@ -39,9 +40,9 @@ def percent_of_votes(v):
         vote_percent = (v[key]/tally) * 100
         print(f"{key}:  {round(vote_percent)}.000%  ({v[key]})")
 
-# get the winner by looping through the vote_count dictionary to compare vote totals and tben top vote getter into a new dictionary
+
+# get the winner by looping through the vote_count dictionary to compare vote totals
 # Got help from https://realpython.com/iterate-through-dictionary-python/#a-few-words-on-dictionaries
-top_vote_getter = {}
 winner_votes = 0
 
 for key, value in vote_count.items():
@@ -52,19 +53,19 @@ for key, value in vote_count.items():
 # Set variable for output file
 output_file = os.path.join("analysis", "analysis.txt")
 
-#  Open the output file
+#  Open the output file and populate it with results
 with open(output_file, "w") as datafile:
     datafile.write("Election Results \n")
     datafile.write("-----------------------------\n")
     datafile.write("Total Votes:  " + str(tally) + "\n")
     datafile.write("-----------------------------\n")
-    #for line in 
-    #datafile.writelines(percent_of_votes(vote_count))
+    for key, value in vote_count.items(): # got help from https://www.quora.com/How-do-I-write-a-dictionary-to-a-file-in-Python for the candidate stats portion
+        percent_votes = (int(value)/tally) * 100
+        percent_votes = round(percent_votes) # rounding didn't work on the line above so added separate line to do that
+        datafile.write('%s:  %d.000%% (%s)\n' % (key, percent_votes, value))
     datafile.write("-----------------------------\n")
     datafile.write("Winner:  " + winner + "\n")
     datafile.write("-----------------------------\n")
-
-
 
 
 # Display the results in the Terminal
@@ -72,8 +73,6 @@ print("Election Results")
 print("-----------------------------")
 print(f"Total Votes:  {tally}")
 print("-----------------------------")
-# for key, value in vote_count.items(): 
-#     print(f"{key}:  ({value})")
 percent_of_votes(vote_count)
 print("-----------------------------")
 print(f"Winner:  {winner}")
