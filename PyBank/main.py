7216d221e1b3b2328c637997a5991220034c42ea
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-07-12 22:39:12 -0700

    final script including printing results to txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,7 +13,7 @@ current_profit_inc = 0
 current_profit_dec = 0
 
 # Open the CSV file
-with open(csvpath) as csvfile:
+with open(csvpath, encoding='utf-8') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=",")
      csv_header = next(csvreader)
 
@@ -35,15 +35,29 @@ with open(csvpath) as csvfile:
 # populate list for greatest increase in profits and greatest decrease in losses
 greatest_profit_loss_list = [current_profit_inc_month, current_profit_inc, current_profit_dec_month, current_profit_dec]
 
-months
-net_total
+# get the final profit/loss and calculate the average change
 last_profit_loss = int_net_total
 avg_changes = (last_profit_loss - first_profit_loss)/(months - 1)
 
+# Set variable for the output file
+output_file = os.path.join("analysis", "analysis.txt")
+
+#  Open the output file and populate it with results
+with open(output_file, "w") as datafile:
+    datafile.write("Financial Analysis \n")
+    datafile.write("-----------------------------\n")
+    datafile.write("Total Months:  " + str(months) + "\n")
+    datafile.write("Total:  $" + str(net_total) + "\n")
+    datafile.write("Average Change:  $" + str(round(avg_changes, 2)) + "\n")
+    datafile.write(f"Greatest Increase in Profits:  {greatest_profit_loss_list[0]} (${greatest_profit_loss_list[1]}) \n")
+    datafile.write(f"Greatest Decrease in Profits:  {greatest_profit_loss_list[2]} (${greatest_profit_loss_list[3]}) \n")
+
+
+# Display the results in the Terminal
 print("Financial Analysis")
 print("--------------------------")
 print(f"Total Months:  {months}")
 print(f"Total:  ${net_total}")
-print("Average Change:  $" + "{:.2f}".format(avg_changes)) # Got formatting help from https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-30.php
+print("Average Change:  $" + str(round(avg_changes, 2)))
 print(f"Greatest Increase in Profits:  {greatest_profit_loss_list[0]} (${greatest_profit_loss_list[1]})")
 print(f"Greatest Decrease in Profits:  {greatest_profit_loss_list[2]} (${greatest_profit_loss_list[3]})")
